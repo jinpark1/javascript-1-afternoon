30b84e5ee5daccd90a46814ef18c1f95b717778f
unknown <jempsey@gmail.com>
2018-05-01 19:54:17 -0700

    javascript-1-afternoon

--- practice.js
+++ practice.js
@@ -188,14 +188,19 @@ let pondScope = ["duck", "realDuck"];
 //function which returns your name.
 
 //Code Here
-
+function outerFn(){
+  return function(){
+    return 'Jin Park';
+  };
+};
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
 //Code Here
-//var innerFn = outerFn();
+// var innerFn = outerFn();
+var innerFn = outerFn();
 
 //Now invoke innerFn and save the result to a variable called finalResult.
 
 //Code Here
-//var finalResult = innerFn;
+var finalResult = innerFn();
